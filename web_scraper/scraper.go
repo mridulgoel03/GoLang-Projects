c3b28b21ec82ca4701de928818532cec0046845d
Mridul Goel <goel.mridul555@gmail.com>
2023-08-14 00:45:57 +0530

    Webscrapper

---
+++ web_scraper/scraper.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	url := "https://example.com"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	document.Find("h2").Each(func(index int, element *goquery.Selection) {
+		title := element.Text()
+		fmt.Printf("Title %d: %s\n", index+1, title)
+	})
+
+	document.Find("p").Each(func(index int, element *goquery.Selection) {
+		paragraph := element.Text()
+		fmt.Printf("Paragraph %d: %s\n", index+1, paragraph)
+	})
+}
